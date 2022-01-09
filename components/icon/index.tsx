b6f9f24407c6dd00d4566a3a8900b8f18f990352
dgvalerio <al.davigvalerio@gmail.com>
2022-01-09 16:27:04 -0300

    feat: add "speedometer" component icon

--- components/icon/index.tsx
+++ components/icon/index.tsx
@@ -4,6 +4,7 @@ import ArrowRight from './arrow-right';
 import History from './history';
 import Key from './key';
 import SignIn from './signin';
+import Speedometer from './speedometer';
 import User from './user';
 
 // Fonts searched in https://iconmonstr.com/
@@ -37,9 +38,10 @@ type IconType = {
   History: FC<IconProps>;
   Key: FC<IconProps>;
   SignIn: FC<IconProps>;
+  Speedometer: FC<IconProps>;
   User: FC<IconProps>;
 };
 
-const Index: IconType = { ArrowRight, History, Key, SignIn, User };
+const Index: IconType = { ArrowRight, History, Key, SignIn, Speedometer, User };
 
 export default Index;
