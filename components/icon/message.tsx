9a1caa1d2c8db43444422f238031dbe0a43d677b
dgvalerio <al.davigvalerio@gmail.com>
2022-01-09 22:09:33 -0300

    refactor: change "message" component icon

--- components/icon/message.tsx
+++ components/icon/message.tsx
@@ -6,7 +6,7 @@ import { IconProps, IconWrapper } from './index';
 
 const MessageIcon: FC<IconProps> = ({ size, width, height }) => (
   <IconWrapper size={size} width={width} height={height}>
-    <path d="M24 21h-24v-18h24v18zm-23-16.477v15.477h22v-15.477l-10.999 10-11.001-10zm21.089-.523h-20.176l10.088 9.171 10.088-9.171z" />
+    <path d="M12 12.713l-11.985-9.713h23.97l-11.985 9.713zm0 2.574l-12-9.725v15.438h24v-15.438l-12 9.725z" />
   </IconWrapper>
 );
 
